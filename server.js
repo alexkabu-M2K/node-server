ccaa1a76d344c7850384e85e7ab542755079aafb
Alexkabu <alexkabu@outlook.com>
2025-08-30 11:09:51 -0300

    Desenvolvimento do Código

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 // Importando módulo node http 
-const http = require('http');
+import http from 'node:http';
 
-http.createServer((request, response) => {
-  response.end('Hello World');
-}).listen(3333, 'localhost');
+// Variavel Port Server
+const PORT = 3333;
+
+// Criando servidor
+const server = http.createServer((request, response) => {
+
+  const url = request.url;
+
+  response.setHeader('Content-Type', 'text/plain');
+
+  response.end(`requisição na url: ${url}`);
+})
+
+server.listen(PORT, 'localhost', () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
